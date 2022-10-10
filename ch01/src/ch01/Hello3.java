c7e6c2d08f653207bca01f035f1e331563d950c8
KimDanBee <eksql8061@naver.com>
2022-10-10 21:39:25 +0900

    2022-10-10

--- ch01/src/ch01/Hello3.java
+++ ch01/src/ch01/Hello3.java
@@ -1,10 +1,13 @@
 package ch01;
 
- class Hello2 {
 
-}
  class Hello3{
 	 public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("3+5");
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3);
+		
 	}
  }
